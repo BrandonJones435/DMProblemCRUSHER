8459d1972f6ae43393c105e3b2a30f48a8f62794
Brandon Jones <brandonjones435@u.boisestate.edu>
2025-01-29 19:59:59 -0700

    IDK if imma still work on this because the end result is not that exciting

--- DMProblemCrusher.java
+++ DMProblemCrusher.java
@@ -1,6 +1,4 @@
 public class DMProblemCrusher {
     // Store the 2d array of the problem 
     private boolean[][] TruthTable; 
-
-    public class DMProblemCrusher(testFile)
     }
---
+++ DMProblemCrusherInterface.java
@@ -0,0 +1,6 @@
+public interface DMProblemCrusherInterface {
+    /**
+     * Reads the first line of the file SampleTT.csv and stores the variable names in an ArrayList.
+     */
+    void readFile();
+}
